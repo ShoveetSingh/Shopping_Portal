d1b6083f925d18df162b888fffdf79efc488deb2
@Shoveet2003 <shoveetsingh2002@gmail.com>
2024-02-13 00:05:24 +0530

    Login successfull!

--- login.php
+++ login.php
@@ -6,14 +6,16 @@ if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
   
     $username=$_POST['username'];
     $password=$_POST['password'];
-    $sql = "SELECT * FROM usernames WHERE name = '$username' AND password = '$password'";
-    if(executeQuery($conn,$sql)){
+    $sql = "SELECT name,password FROM usernames WHERE name = '$username' AND password = '$password'";
+    $result = executeQuery($conn,$sql);
+    if($result->num_rows>0 && $result!=null){
         echo "Login Successfull!";
         header('Location:details.php');
         exit();
     }
     else{
-        echo "Username or password incorrect!";
+        echo "<script>alert('Username or password unsuccessfull!');</script>";
+        exit();
     }
 }
 
@@ -25,8 +27,8 @@ if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
     <head><title>Login?</title></head>
     <body>
         <form method="post" action="login.php">
-            Username:<input type="text" required><br><br>
-            Password:<input type="password" required><br><br>
+            Username:<input type="text" name="username" required><br><br>
+            Password:<input type="password" name="password" required><br><br>
             <input type="submit" value="Login">
 </form>
         <!-- <input type="submit" value="Delete" > -->
